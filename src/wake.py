718e17a2e7a7401436f48af589b8b358c7b52839
Emad <emad.hosseini@gearset.com>
2024-09-05 15:55:03 +0100

    Wake word basic test

--- src/wake.py
+++ src/wake.py
@@ -1,6 +1,37 @@
+from keys import pico_voice_key
 import pvporcupine
+from pvrecorder import PvRecorder
 
 porcupine = pvporcupine.create(
-  access_key='${ACCESS_KEY}',
-  keywords=['picovoice', 'bumblebee']
+    access_key=f"{pico_voice_key()}",
+    keywords=["bumblebee"],
 )
+
+
+def get_next_audio_frame():
+    return recorder.read()
+
+
+def listen_until_woken():
+    while True:
+        audio_frame = get_next_audio_frame()
+        keyword_index = porcupine.process(audio_frame)
+        if keyword_index == 0:
+            print("Flubber!!!!")
+            porcupine.delete()
+            return
+
+
+# for i, device in enumerate(PvRecorder.get_available_devices()):
+#     print("Device %d: %s" % (i, device))
+recorder = PvRecorder(frame_length=porcupine.frame_length, device_index=0)
+recorder.start()
+
+print("Listening ... (press Ctrl+C to exit)")
+try:
+    listen_until_woken()
+except KeyboardInterrupt:
+    print("Stopping ...")
+finally:
+    recorder.delete()
+    porcupine.delete()
