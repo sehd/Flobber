86c78e56dcda45fc407717ea2a992b12f3e232e5
Emad <emad.hosseini@gearset.com>
2024-09-07 13:38:55 +0100

    Add yes

--- src/tts.py
+++ src/tts.py
@@ -15,9 +15,11 @@ def say_offline(text):
 
 
 def say_openai(text):
-    client = OpenAI(api_secret=openai_api_key())
+    client = OpenAI(api_key=openai_api_key())
     speech_file_path = "output/openai.mp3"
     response = client.audio.speech.create(model="tts-1", voice="alloy", input=text)
 
     response.write_to_file(speech_file_path)
     play(speech_file_path)
+
+# say_openai("Yes?")
