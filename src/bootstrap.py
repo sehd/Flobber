2f1025c116ceede281874019efa7c6152853432c
Emad <emad.hosseini@gearset.com>
2024-09-08 15:16:20 +0100

    Start from bootstrap

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,56 +1,55 @@
-def bootstrap(start_main_loop):
-    print("Starting up...")
+print("Starting up...")
 
-    print("Testing tts...")
+print("Testing tts...")
 
-    import tts
+import tts
 
-    tts.say_offline("Starting up")
+tts.say_offline("Starting up")
 
-    print("Starting mic...")
+print("Starting mic...")
 
-    from mic import Mic, get_mics
-    from wake import Wake
+from mic import Mic, get_mics
+from wake import Wake
 
-    mics = [x for x in get_mics()]
-    print("\n".join(mics))
-    if len(mics) == 0:
-        print("No microphone found.")
-        exit()
+mics = [x for x in get_mics()]
+print("\n".join(mics))
+if len(mics) == 0:
+    print("No microphone found.")
+    exit()
 
-    import settings
+import settings
 
-    print(f"Selected mic: {mics[settings.get_mic_device_id()]}")
-    with Wake() as wake:
-        print(f"Starting wake. Frame length = {wake.get_device_frame_length()}")
-        with Mic(
-            settings.get_mic_device_id(), wake.get_device_frame_length()
-        ) as recorder:
-            if settings.enable_mic_test_on_bootstrap():
-                recorder.start_recorder()
+print(f"Selected mic: {mics[settings.get_mic_device_id()]}")
+with Wake() as wake:
+    print(f"Starting wake. Frame length = {wake.get_device_frame_length()}")
+    with Mic(settings.get_mic_device_id(), wake.get_device_frame_length()) as recorder:
+        if settings.enable_mic_test_on_bootstrap():
+            recorder.start_recorder()
 
-                print("Recording test file... make some noise")
-                testRecordingPath = "output/testRecording.wav"
-                recorder.record_test_file(testRecordingPath)
-                recorder.stop_recorder()
+            print("Recording test file... make some noise")
+            testRecordingPath = "output/testRecording.wav"
+            recorder.record_test_file(testRecordingPath)
+            recorder.stop_recorder()
 
-                print("Playing back")
+            print("Playing back")
 
-                from speak import play
+            from speak import play
 
-                play(testRecordingPath)
-                print(f"Test recording saved in {testRecordingPath}")
+            play(testRecordingPath)
+            print(f"Test recording saved in {testRecordingPath}")
 
-            print("Listening ... (press Ctrl+C to exit)")
-            try:
-                start_main_loop(recorder, wake)
-            except KeyboardInterrupt:
-                tts.say_offline("Flubber shut down")
-                print("Stopping ...")
-            except Exception as ex:
-                if hasattr(ex, "message"):
-                    print(ex.message)
-                    tts.say_offline(ex.message)
-                else:
-                    print(ex)
-                    tts.say_offline(ex)
+        print("Listening ... (press Ctrl+C to exit)")
+        try:
+            from main import start_main_loop
+
+            start_main_loop(recorder, wake)
+        except KeyboardInterrupt:
+            tts.say_offline("Flubber shut down")
+            print("Stopping ...")
+        except Exception as ex:
+            if hasattr(ex, "message"):
+                print(ex.message)
+                tts.say_offline(ex.message)
+            else:
+                print(ex)
+                tts.say_offline(ex)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from stt import transcribe_audio_openai
 from speak import play
-from bootstrap import bootstrap
 from chatgpt import get_chatgpt_response
 from tts import say_openai
 
@@ -19,6 +18,3 @@ def start_main_loop(recorder, wake):
         command = transcribe_audio_openai(command_path)
         gpt_response = get_chatgpt_response(command)
         say_openai(gpt_response)
-
-
-bootstrap(start_main_loop)
