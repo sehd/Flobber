e0885903cc426ef0108da84d58f7fece29be9f70
Emad <emad.hosseini@gearset.com>
2024-10-03 16:55:20 +0100

    First dual monitor try

--- src/eyes.py
+++ src/eyes.py
@@ -6,33 +6,47 @@ sys.path.append(os.getcwd())
 from lib import LCD_1inch28
 
 from PIL import Image
+from spidev import SPI
 
-# Raspberry Pi pin configuration:
-RST = 27
-DC = 25
-BL = 18
-bus = 0
-device = 0
+m0Config = {"RST": 27, "DC": 25, "BL": 6, "bus": 0, "device": 0}
+
+m1Config = {"RST": 22, "DC": 24, "BL": 12, "bus": 1, "device": 1}
 try:
-    # display with hardware SPI:
-    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
-    #display = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
-    display = LCD_1inch28.LCD_1inch28()
+    display0 = LCD_1inch28.LCD_1inch28(
+        spi=SPI.SpiDev(m0Config["bus"], m0Config["device"]),
+        spi_freq=10000000,
+        rst=m0Config["RST"],
+        dc=m0Config["DC"],
+        bl=m0Config["BL"],
+    )
+    display1 = LCD_1inch28.LCD_1inch28(
+        spi=SPI.SpiDev(m1Config["bus"], m1Config["device"]),
+        spi_freq=10000000,
+        rst=m1Config["RST"],
+        dc=m1Config["DC"],
+        bl=m1Config["BL"],
+    )
+    # display = LCD_1inch28.LCD_1inch28()
     # Initialize library.
-    display.Init()
+    display0.Init()
+    display1.Init()
     # Clear display.
-    display.clear()
+    display0.clear()
+    display1.clear()
     while True:
-        image = Image.open('assets/eyes/LeftEyeOpen.jpg')
-        display.ShowImage(image)
+        image = Image.open("assets/eyes/LeftEyeOpen.jpg")
+        display0.ShowImage(image)
+        display1.ShowImage(image)
         time.sleep(3)
-        image = Image.open('assets/eyes/LeftEyeClose.jpg')
-        display.ShowImage(image)
+        image = Image.open("assets/eyes/LeftEyeClose.jpg")
+        display0.ShowImage(image)
+        display1.ShowImage(image)
         time.sleep(0.2)
 except IOError as e:
     print("Error in eyes module:")
     print(e)
 except KeyboardInterrupt:
-    display.clear()
-    display.module_exit()
-    
+    display0.clear()
+    display1.clear()
+    display0.module_exit()
+    display1.module_exit()
