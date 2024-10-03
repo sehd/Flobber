fe610ab3c28e9a7fbada460b7a4e7ad8a57d9f73
Emad <emad.hosseini@gearset.com>
2024-10-03 17:31:17 +0100

    Try two

--- src/eyes.py
+++ src/eyes.py
@@ -1,32 +1,21 @@
 import time
 import sys
 import os
+import spidev as SPI
+from PIL import Image
 
 sys.path.append(os.getcwd())
 from lib import LCD_1inch28
+from lib.lcdconfig import RaspberryPi
 
-from PIL import Image
-from spidev import SPI
-
-m0Config = {"RST": 27, "DC": 25, "BL": 6, "bus": 0, "device": 0}
-
-m1Config = {"RST": 22, "DC": 24, "BL": 12, "bus": 1, "device": 1}
 try:
     display0 = LCD_1inch28.LCD_1inch28(
-        spi=SPI.SpiDev(m0Config["bus"], m0Config["device"]),
-        spi_freq=10000000,
-        rst=m0Config["RST"],
-        dc=m0Config["DC"],
-        bl=m0Config["BL"],
+        RaspberryPi(spi=SPI.SpiDev(0, 0), rst=27, dc=25, bl=6)
     )
     display1 = LCD_1inch28.LCD_1inch28(
-        spi=SPI.SpiDev(m1Config["bus"], m1Config["device"]),
-        spi_freq=10000000,
-        rst=m1Config["RST"],
-        dc=m1Config["DC"],
-        bl=m1Config["BL"],
+        RaspberryPi(spi=SPI.SpiDev(1, 1), rst=22, dc=24, bl=12)
     )
-    # display = LCD_1inch28.LCD_1inch28()
+
     # Initialize library.
     display0.Init()
     display1.Init()
