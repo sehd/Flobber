b649fe791fbad80d1aedf8b97262a556e1b0e34d
Emad Hosseini <emad.hosseini@gearset.com>
2025-11-04 12:50:16 +0000

    Fix test

--- tests/manual/eyes.py
+++ tests/manual/eyes.py
@@ -1,8 +1,8 @@
-from ...src.eyes import Eyes, EyeStates
+from src.eyes import Eyes, EyeStates
 
 state = input("Enter eye state (Off, Open, Close, Blinking, Heart): ")
 
 with Eyes() as eyes:
-    while state not in EyeStates.__members__:
-        eyes.set_state(state)
+    while state in EyeStates.__members__:
+        eyes.set_state(EyeStates[state])
         state = input("Enter eye state (Off, Open, Close, Blinking, Heart): ")
