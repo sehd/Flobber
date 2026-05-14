a913f829080d38b323822e304c625d244bcb5433
Emad Hosseini <emad.hosseini@gearset.com>
2026-05-14 16:04:55 +0100

    Fix eye tester

--- tests/manual/eyes.py
+++ tests/manual/eyes.py
@@ -7,14 +7,14 @@ request = input("Enter eye state (Off, Open, Blinking): ")
 def get_state(request: str):
     if request.lower() == "open":
         return FullOpen()
-    if request.lower() == "Off":
+    if request.lower() == "off":
         return Off()
-    if request.lower() == "Blinking":
-        return Blinking
+    if request.lower() == "blinking":
+        return Blinking()
     return None
 
 
 with Eyes() as eyes:
-    while get_state(request is not None):
+    while get_state(request) is not None:
         eyes.set_state(get_state(request))
-        state = input("Enter eye state (Off, Open, Close, Blinking, Heart): ")
+        request = input("Enter eye state (Off, Open, Blinking): ")
